0b048aaaa412b5812e6b0c3d29cb0387fe269bbd
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-29 22:06:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,12 @@ class Trip {
     }
     store.trips.push(this)
   }
-
+  setDriver(driver){
+    this.driverId = driver.id
+  }
+  setPassenger(passenger){
+    this.passengerId = passenger.id
+  }
   driver(){
     return store.drivers.find(function(driver){
       return driver.id === this.driverId
